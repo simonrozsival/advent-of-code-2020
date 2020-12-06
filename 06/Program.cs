b9539ef851d7030f329c0164bb5a6ffa2ff21679
Simon Rozsival <simon@rozsival.com>
2020-12-06 09:56:22 +0100

    :lipstick: Final C# solution

--- 06/Program.cs
+++ 06/Program.cs
@@ -3,29 +3,23 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 
-var inputFile = new StreamReader("./in.txt");
+using var inputFile = new StreamReader("./in.txt");
 var input = inputFile.ReadToEnd()
-  .Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
-  .Select(group => group.Split("\n", StringSplitOptions.RemoveEmptyEntries))
-  .ToArray();
+    .Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+    .Select(group => group.Split("\n", StringSplitOptions.RemoveEmptyEntries))
+    .ToArray();
 
 Console.WriteLine($"Puzzle 1: {solvePuzzle1(input)}");
 Console.WriteLine($"Puzzle 2: {solvePuzzle2(input)}");
 
 int solvePuzzle1(IEnumerable<string[]> groups)
-  => groups.Select(union).Sum(union => union.Count);
+    => groups.Select(group => group.Aggregate(
+            seed: Enumerable.Empty<char>(),
+            func: (acc, next) => acc.Union(next)))
+        .Sum(union => union.Count());
 
 int solvePuzzle2(IEnumerable<string[]> groups)
-  => groups.Select(intersect).Sum(intersection => intersection.Length);
-
-HashSet<char> union(string[] group)
-  => group.Aggregate(seed: new HashSet<char>(), unionTwo);
-
-HashSet<char> unionTwo(HashSet<char> acc, string next)
-  => acc.Union(next.ToCharArray()).ToHashSet();
-
-char[] intersect(string[] group)
-  => group.Aggregate(seed: group[0].ToCharArray(), intersectTwo);
-
-char[] intersectTwo(char[] acc, string next)
-  => acc.Where(c => next.Contains(c)).ToArray();
+    => groups.Select(group => group.Aggregate(
+            seed: group.FirstOrDefault() ?? Enumerable.Empty<char>(),
+            func: (acc, next) => acc.Where(next.Contains)))
+        .Sum(intersection => intersection.Count());
